8c8c21a64cbd0a48d51312bb986b4c0710ba3314
gxiaosong <lyricsong47@gmail.com>
2024-10-18 00:11:54 +0800

    Merge branch 'refs/heads/shopware-trunk' into trunk

---
+++ Product/ProductUpdater.php
@@ -0,0 +1,47 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Elasticsearch\Product;
+
+use Shopware\Core\Content\Product\Events\ProductIndexerEvent;
+use Shopware\Core\Content\Product\Events\ProductStockAlteredEvent;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityDefinition;
+use Shopware\Core\Framework\Log\Package;
+use Shopware\Elasticsearch\Framework\Indexing\ElasticsearchIndexer;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * @internal
+ */
+#[Package('core')]
+class ProductUpdater implements EventSubscriberInterface
+{
+    /**
+     * @internal
+     */
+    public function __construct(
+        private readonly ElasticsearchIndexer $indexer,
+        private readonly EntityDefinition $definition
+    ) {
+    }
+
+    /**
+     * @return array<string, string|array{0: string, 1: int}|list<array{0: string, 1?: int}>>
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            ProductIndexerEvent::class => 'update',
+            ProductStockAlteredEvent::class => 'stockUpdate',
+        ];
+    }
+
+    public function update(ProductIndexerEvent $event): void
+    {
+        $this->indexer->updateIds($this->definition, $event->getIds());
+    }
+
+    public function stockUpdate(ProductStockAlteredEvent $event): void
+    {
+        $this->indexer->updateIds($this->definition, $event->getIds());
+    }
+}
